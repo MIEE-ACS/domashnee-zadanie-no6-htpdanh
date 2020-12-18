b7cfe86c286b1d3d262ed27987471d85bf7f6728
htpdanh <70977838+htpdanh@users.noreply.github.com>
2020-12-18 19:13:55 +0300

    Задание выполнено

---
+++ DZ6_WPF/DZ6_WPF/Figure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ6_WPF
+{
+    abstract class Figure
+    {
+        public virtual double Area() { return 0; }
+        public virtual double Perimeter() { return 0; }
+    }
+}
